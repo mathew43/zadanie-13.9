a0a2ff1242849e83e33004dd0c614e2098dfd40d
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2018-02-07 19:12:50 +0100

    zadanie

--- modules/server.js
+++ modules/server.js
@@ -21,11 +21,11 @@ function start() {
         default:
             handlers.error(request, response);
     }
-  }
+}
 
   http.createServer(onRequest).listen(9000);
-
-  console.log("Uruchomiono serwer!".green);
 }
+  console.log("Uruchomiono serwer!".green);
+
 
 exports.start = start;
